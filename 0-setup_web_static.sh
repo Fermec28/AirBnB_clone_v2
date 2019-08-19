cb59dfa19ad8ee899ffd4b257c0d988c2d66f593
Jose Medina <fermed28@gmail.com>
2019-08-19 23:38:23 +0000

    setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static
+if [ -x /usr/sbin/nginx ]
+then
+    sudo apt-get update
+    sudo apt-get -y install nginx
+fi
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "Hello holberton" >> /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i "51i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
+service nginx restart 
