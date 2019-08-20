27b44c2963d71dab28c4bfbf5b60f775d09cc9ea
Jose Medina <fermed28@gmail.com>
2019-08-20 01:25:53 +0000

    compressed web static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" versioning managment """
+from fabric.api import*
+from datetime import datetime
+
+
+def do_pack():
+    now = datetime.now()
+    current_time = now.strftime("%Y%m%d%H%M")
+    name = "versions/web_static_{}.tgz web_static".format(current_time)
+    local('mkdir -p versions')
+    result = local("tar -cvzf {}".format(name))
+    if result.return_code == 0:
+        return name
+    else:
+        return None
