366122a71dbcf1174a1ee499994c37fa1cfddc5b
Jose Medina <fermed28@gmail.com>
2019-08-20 05:04:06 +0000

    [fixes] fixing shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-if [ !-x /usr/sbin/nginx ]
+if [ ! -x /usr/sbin/nginx ]
 then
     sudo apt-get update
     sudo apt-get -y install nginx
 fi
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "Hello holberton" | sudo tee  /data/web_static/releases/test/index.html
+echo "Hello holberton" | sudo tee  /data/web_static/releases/test/index.html > /dev/null
 ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i "51i\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-available/default
